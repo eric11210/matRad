7f962820293f8b81c4b75441415886bd0153abd7
eric11210 <ericchristiansen@cmail.carleton.ca>
2020-05-12 18:03:45 -0400

    Script to create 4DDose figures
    Update script to create 4DDose figures for LaTeX.

--- 4D Dose Calc Validation/show4DDose.m
+++ 4D Dose Calc Validation/show4DDose.m
@@ -1,25 +1,55 @@
 w = zeros(dij.totalNumOfBixels,1);
-w(90) = 1;
+w(110) = 1;
+%w(66) = 1;
 
 physicalDose = cell(dij.numOfScenarios,1);
 
-for i = 1:dij.numOfScenarios
+for i = 1:dij.numPhases
     
     physicalDose{i} = dij.physicalDose{i}*w;
     physicalDose{i} = reshape(physicalDose{i},dij.dimensions);
     
-    imCt = imrotate(squeeze(ct.cube{i}(:,56,:)),90);
-    imDose = imrotate(squeeze(physicalDose{i}(:,56,:)),90);
-    figure
-    imagesc(imCt)
-    hold on
-    super = imagesc(imDose);
-    set(super,'AlphaData',0.5);
+    imCt    = imrotate(squeeze(ct.cubeHU{1}(:,53,:)),90);
+    imDose  = imrotate(squeeze(physicalDose{i}(:,53,:)),90);
+    alpha   = 1.*(imDose > 0);
     
-    title(sprintf('Phase %d of %d',i,dij.numOfScenarios));
-    xlabel('Increasing posterior voxel')
-    ylabel('Increasing inferior voxel')
+    x = [ct.y(1) ct.y(end)];
+    y = [ct.z(1) ct.z(end)];
     
-    savefig(sprintf('Phase %d of %d',i,dij.numOfScenarios));
+    % create figure
+    figure;
+    % create two axes
+    ax1 = axes;
+    ax2 = axes;
+    % put the images in the axes
+    imagesc(ax1,x,y,imCt)
+    imagesc(ax2,x,y,imDose,'AlphaData',alpha)
+    % hide the top axes
+    ax2.Visible = 'off';
+    ax2.XTick = [];
+    ax2.YTick = [];
+    % give each one its own colormap
+    colormap(ax1,'gray')
+    colormap(ax2,'default')
+    % set aspect ratio
+    ax1.DataAspectRatio = [1 1 1];
+    ax2.DataAspectRatio = [1 1 1];
+    
+    % then add colorbars and get everything lined up
+    cb1 = colorbar(ax1,'WestOutside');
+    cb2 = colorbar(ax2,'EastOutside');
+    set([ax1 ax2],'Position',mean([ax1.Position; ax2.Position],1));
+    
+    caxis(ax1,[-1000 600])
+    caxis(ax2,[0 0.6])
+    
+    %title(sprintf('Phase %d of %d',i,dij.numPhases));
+    xlabel(ax1,'posterior position / simm')
+    ylabel(ax1,'inferior position / simm')
+    
+    %set(gcf, 'Colormap', gray);
+    
+    %savefig(sprintf('Phase %d of %d',i,dij.numOfScenarios));
     pause(1)
+    
 end
