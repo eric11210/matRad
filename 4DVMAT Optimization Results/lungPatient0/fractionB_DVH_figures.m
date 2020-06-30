97edee08096a55a8e7d2b60fb013f7332935de79
eric11210 <ericchristiansen@cmail.carleton.ca>
2020-06-30 10:27:28 -0400

    Fraction intercomparison results
    Include the results of the fraction A-B intercomparison study (on the dose).

---
+++ 4DVMAT Optimization Results/lungPatient0/fractionB_DVH_figures.m
@@ -0,0 +1,111 @@
+%% setup
+
+load lungPatient0_5mm5p_rep
+
+% meta information for treatment plan
+
+pln.radiationMode   = 'photons';   % either photons / protons / carbon
+pln.machine         = 'Generic';
+
+% beam geometry settings
+pln.propStf.bixelWidth = 5;
+
+% dose calculation settings
+pln.propDoseCalc.marginOptions.addMargin    = true; % margin around targets for determining which bixels to calculate dose
+pln.propDoseCalc.marginOptions.margin.x     = 10; % margin size in mm
+pln.propDoseCalc.marginOptions.margin.y     = 10; % margin size in mm
+pln.propDoseCalc.marginOptions.margin.z     = 10; % margin size in mm
+pln.propDoseCalc.memorySaverPhoton          = false;
+pln.propDoseCalc.vmc                        = true;
+pln.propDoseCalc.vmcOptions.keepError       = false;
+pln.propDoseCalc.vmcOptions.source          = 'phsp';
+pln.propDoseCalc.vmcOptions.phspBaseName    = '5cmx5cm_SSD50cm';
+pln.propDoseCalc.vmcOptions.SCD             = 500;
+pln.propDoseCalc.vmcOptions.dumpDose        = 1;
+pln.propDoseCalc.vmcOptions.version         = 'Carleton';
+pln.propDoseCalc.vmcOptions.nCasePerBixel   = 500;
+pln.propDoseCalc.vmcOptions.numOfParMCSim   = 16;
+pln.propDoseCalc.sampleTargetProb           = 1;
+
+% optimization settings
+pln.propOpt.bioOptimization = 'none';
+pln.propOpt.runVMAT         = true;
+pln.propOpt.runDAO          = true;
+pln.propOpt.runSequencing   = true;
+pln.propOpt.preconditioner  = true;
+pln.propOpt.numLevels       = 7;
+
+pln.propOpt.VMAToptions.machineConstraintFile   = [pln.radiationMode '_' pln.machine];
+pln.propOpt.VMAToptions.continuousAperture      = true;
+pln.propOpt.VMAToptions.fixedGantrySpeed        = true;
+pln.propOpt.VMAToptions.deliveryTime            = 70;
+
+pln.propOpt.VMAToptions.startingAngle               = -180;
+pln.propOpt.VMAToptions.finishingAngle              = 180;
+pln.propOpt.VMAToptions.maxGantryAngleSpacing       = 4;      % Max gantry angle spacing for dose calculation
+pln.propOpt.VMAToptions.maxFluGantryAngleSpacing    = 4;    % Max gantry angle spacing for fluence calculation
+pln.propOpt.VMAToptions.maxDAOGantryAngleSpacing    = 8;      % Max gantry angle spacing for DAO
+pln.propOpt.VMAToptions.maxFMOGantryAngleSpacing    = 32;      % Max gantry angle spacing for FMO
+
+pln.propOpt.run4D                   = true;
+pln.propOpt.varOpt                  = false;
+pln.propOpt.prop4D.singlePhaseFMO   = true;
+% multi-phase FMO hasn't been implemented fully (would have to do changes in FMO and leaf
+% sequencing - probably better only for fluence, not DAO).
+
+pln = matRad_VMATGantryAngles(pln,cst,ct);
+
+numOfFractions = pln.numOfFractions;
+
+% absolute path to figure folder
+figurePath = 'C:\Users\eric\Carleton University\OneDrive - Carleton University\Carleton\PhD Project\Thesis\figures\generator\';
+
+%% CO
+
+load('fractionB_CO')
+pln.numOfFractions = numOfFractions;
+
+matRad_showDVHBands(dvh,pdvh_MC,[1 3 5],cst,pln);
+
+% save figure
+fname = 'lungPatient1_fractionB_CO_DVH_raw';
+fullpath = [figurePath fname '.tex'];
+matlab2tikz('filename',fullpath,'interpretTickLabelsAsTex',true,'parseStrings',false,'noSize',true,'extraAxisOptions','minor y tick num=3,minor x tick num=3,width=\linewidth,height=\linewidth','showInfo',false);
+
+%% STO
+
+load('fractionB_STO')
+pln.numOfFractions = numOfFractions;
+
+matRad_showDVHBands(dvh,pdvh_MC,[1 3 5],cst,pln);
+
+% save figure
+fname = 'lungPatient1_fractionB_STO_DVH_raw';
+fullpath = [figurePath fname '.tex'];
+matlab2tikz('filename',fullpath,'interpretTickLabelsAsTex',true,'parseStrings',false,'noSize',true,'extraAxisOptions','minor y tick num=3,minor x tick num=3,width=\linewidth,height=\linewidth','showInfo',false);
+
+%% 3DITV
+
+load('fractionB_3DITV')
+pln.numOfFractions = numOfFractions;
+
+matRad_showDVHBands(dvh,pdvh_MC,[1 3 5],cst,pln);
+
+% save figure
+fname = 'lungPatient1_fractionB_3DITV_DVH_raw';
+fullpath = [figurePath fname '.tex'];
+matlab2tikz('filename',fullpath,'interpretTickLabelsAsTex',true,'parseStrings',false,'noSize',true,'extraAxisOptions','minor y tick num=3,minor x tick num=3,width=\linewidth,height=\linewidth','showInfo',false);
+
+%% COopt_recalcA
+
+load('fractionB_COopt_recalcA')
+pln.numOfFractions = numOfFractions;
+
+matRad_showDVHBands(dvh,pdvh_MC,[1 3 5],cst,pln);
+
+% save figure
+fname = 'lungPatient1_fractionB_COopt_recalcA_DVH_raw';
+fullpath = [figurePath fname '.tex'];
+matlab2tikz('filename',fullpath,'interpretTickLabelsAsTex',true,'parseStrings',false,'noSize',true,'extraAxisOptions','minor y tick num=3,minor x tick num=3,width=\linewidth,height=\linewidth','showInfo',false);
+
+
---
+++ 4DVMAT Optimization Results/lungPatient0/fractionB_intercomparison.m
@@ -0,0 +1,119 @@
+%% setup
+
+load lungPatient0_5mm5p_rep
+
+currentDir = pwd;
+
+% meta information for treatment plan
+
+pln.radiationMode   = 'photons';   % either photons / protons / carbon
+pln.machine         = 'Generic';
+
+% beam geometry settings
+pln.propStf.bixelWidth = 5;
+
+% dose calculation settings
+pln.propDoseCalc.marginOptions.addMargin    = true; % margin around targets for determining which bixels to calculate dose
+pln.propDoseCalc.marginOptions.margin.x     = 10; % margin size in mm
+pln.propDoseCalc.marginOptions.margin.y     = 10; % margin size in mm
+pln.propDoseCalc.marginOptions.margin.z     = 10; % margin size in mm
+pln.propDoseCalc.memorySaverPhoton          = false;
+pln.propDoseCalc.vmc                        = true;
+pln.propDoseCalc.vmcOptions.keepError       = false;
+pln.propDoseCalc.vmcOptions.source          = 'phsp';
+pln.propDoseCalc.vmcOptions.phspBaseName    = '5cmx5cm_SSD50cm';
+pln.propDoseCalc.vmcOptions.SCD             = 500;
+pln.propDoseCalc.vmcOptions.dumpDose        = 1;
+pln.propDoseCalc.vmcOptions.version         = 'Carleton';
+pln.propDoseCalc.vmcOptions.nCasePerBixel   = 500;
+pln.propDoseCalc.vmcOptions.numOfParMCSim   = 16;
+pln.propDoseCalc.sampleTargetProb           = 1;
+
+% optimization settings
+pln.propOpt.bioOptimization = 'none';
+pln.propOpt.runVMAT         = true;
+pln.propOpt.runDAO          = true;
+pln.propOpt.runSequencing   = true;
+pln.propOpt.preconditioner  = true;
+pln.propOpt.numLevels       = 7;
+
+pln.propOpt.VMAToptions.machineConstraintFile   = [pln.radiationMode '_' pln.machine];
+pln.propOpt.VMAToptions.continuousAperture      = true;
+pln.propOpt.VMAToptions.fixedGantrySpeed        = true;
+pln.propOpt.VMAToptions.deliveryTime            = 70;
+
+pln.propOpt.VMAToptions.startingAngle               = -180;
+pln.propOpt.VMAToptions.finishingAngle              = 180;
+pln.propOpt.VMAToptions.maxGantryAngleSpacing       = 4;      % Max gantry angle spacing for dose calculation
+pln.propOpt.VMAToptions.maxFluGantryAngleSpacing    = 4;    % Max gantry angle spacing for fluence calculation
+pln.propOpt.VMAToptions.maxDAOGantryAngleSpacing    = 8;      % Max gantry angle spacing for DAO
+pln.propOpt.VMAToptions.maxFMOGantryAngleSpacing    = 32;      % Max gantry angle spacing for FMO
+
+pln.propOpt.run4D                   = true;
+pln.propOpt.varOpt                  = false;
+pln.propOpt.prop4D.singlePhaseFMO   = true;
+% multi-phase FMO hasn't been implemented fully (would have to do changes in FMO and leaf
+% sequencing - probably better only for fluence, not DAO).
+
+pln = matRad_VMATGantryAngles(pln,cst,ct);
+
+% change model
+load('fractionB','model')
+pln.propOpt.prop4D.motionModel = model;
+
+stf = matRad_generateStf(ct,cst,pln);
+
+%% calculate (load) dij
+
+%dij = matRad_calcPhotonDoseVmc(ct,stf,pln,cst);
+dij = matRad_loadDij('lungPatient0_5mm5p_rep');
+dij.numOfFractions = pln.numOfFractions;
+
+%% CO
+
+% load in resultGUI
+load('CO','resultGUI')
+
+% change model
+resultGUI.apertureInfo.motionModel = matRad_prepModelForOpt(pln,stf,resultGUI.apertureInfo);
+
+% do dvhs
+[pdvh_MC,dvh_mean_MC,dvh_std_MC] = matRad_dvhMC(resultGUI.apertureInfo,dij,cst,pln,100);
+[dvh,~] = matRad_indicatorWrapper(cst,pln,resultGUI);
+
+% save results
+save('fractionB_CO','resultGUI','*dvh*');
+
+%% STO
+
+% load in resultGUI
+load('STO','resultGUI')
+
+% change model
+resultGUI.apertureInfo.motionModel = matRad_prepModelForOpt(pln,stf,resultGUI.apertureInfo);
+
+% do dvhs
+[pdvh_MC,dvh_mean_MC,dvh_std_MC] = matRad_dvhMC(resultGUI.apertureInfo,dij,cst,pln,100);
+[dvh,~] = matRad_indicatorWrapper(cst,pln,resultGUI);
+
+% save results
+save('fractionB_STO','resultGUI','*dvh*');
+
+
+%% 3DITV
+
+% load in resultGUI
+load('3DITV','resultGUI')
+
+% change model
+resultGUI.apertureInfo.motionModel = matRad_prepModelForOpt(pln,stf,resultGUI.apertureInfo);
+
+% change initProb
+resultGUI.apertureInfo.motionModel.initProb = resultGUI.apertureInfo.motionModel.Pi_deltaTSample';
+
+% do dvhs
+[pdvh_MC,dvh_mean_MC,dvh_std_MC] = matRad_dvhMC(resultGUI.apertureInfo,dij,cst,pln,100);
+[dvh,~] = matRad_indicatorWrapper(cst,pln,resultGUI);
+
+% save results
+save('fractionB_3DITV','resultGUI','*dvh*');
